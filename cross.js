69429169c73df84b341f7f946b359c4bb4976cc2
keiranking <me@keiranking.com>
2017-08-31 14:26:06 -0400

    Code cleanup.

--- cross.js
+++ cross.js
@@ -18,13 +18,11 @@ window.addEventListener('keydown', function (e) {
         activeCell.className = (activeCell.className + " black").trim();
     } else if (e.which == 8) {
         activeCell.innerHTML = undefined;
-        activeCell.className = activeCell.className.replace("black", "").trim();        
+        activeCell.className = activeCell.className.replace("black", "").trim();
     }
     console.log(activeCell.innerHTML);
   }
 });
-// const grid = document.getElementById("grid");
-// grid.addEventListener('keydown', updateSquare(e));
 
 function updateCursor() {
   const previousCell = document.getElementById("grid").querySelector('[data-row="' + activeRow + '"]').querySelector('[data-col="' + activeCol + '"]');
@@ -39,52 +37,6 @@ function updateCursor() {
   activeCell.className = (activeCell.className + " active").trim();
 }
 
-// function updateSquare(e) {
-//   if (e.keyCode == 90) {
-//     document.getElementById("grid").querySelector('[data-row="' + activeRow + '"]').querySelector('[data-col="' + activeCol + '"]').innerHTML = e;
-//   }
-// }
 
-// Writing into an HTML element, using innerHTML.
-// Writing into the HTML output using document.write().
-// Writing into an alert box, using window.alert().
-// Writing into the browser console, using console.log().
-
-/*
-var isOff = true;
-function light() {
-    var pic;
-    if (isOff == true) {
-        pic = "https://www.w3schools.com/html/pic_bulbon.gif"
-        isOff = false;
-    } else {
-        pic = "https://www.w3schools.com/html/pic_bulboff.gif"
-        isOff = true;
-    }
-    document.getElementById('myImage').src = pic;
-    console.log("Switch flipped.")
-}
-
-function shrink() {
-	document.getElementById('myImage').width = document.getElementById('myImage').width * .9
-	document.getElementById('myImage').height = document.getElementById('myImage').height * .9
-  console.log("Less light.")
-}
-*/
-
-/*
-document.getElementById('math').innerHTML = Math.floor((Math.random() * 12) + 1);
-window.alert("This is how you create an alert.")
-document.write("This is how you write to the HTML document.")
-console.log("This is how you write to the console (for debugging).");
-
-var x, y;
-x = "5";
-y = 5.0;
-if (x == y) {
-  console.log("x and y are equal value");
-}
-if (x === y) {
-  console.log("x and y are equal value and type");
-}
-*/
+// window.alert("This is how you create an alert.")
+// document.write("This is how you write to the HTML document.")
