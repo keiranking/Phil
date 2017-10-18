1129b537346517bf6050b1121fc09cd72b312b02
keiranking <me@keiranking.com>
2017-10-18 11:31:22 -0400

    Added license file and source code header.

--- cross.js
+++ cross.js
@@ -1,3 +1,18 @@
+// Phil
+// ------------------------------------------------------------------------
+// Copyright 2017 Keiran King
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// (https://www.apache.org/licenses/LICENSE-2.0)
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ------------------------------------------------------------------------
+
 const keyboard = {
   "a":      65, "b": 66, "c": 67, "d": 68, "e": 69, "f": 70, "g": 71, "h": 72,
   "i":      73, "j": 74, "k": 75, "l": 76, "m": 77, "n": 78, "o": 79, "p": 80,
