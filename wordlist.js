ea43f3ab44893dcccb6a9c4eb34aec7c4d52edcc
John Costanzo <John.Costanzo@dowjones.com>
2019-08-22 13:40:37 -0400

    Indicate "harmonious" word pairs; implemeted Undo; fixed numbering defect

--- wordlist.js
+++ wordlist.js
@@ -14,9 +14,9 @@
 // ------------------------------------------------------------------------
 
 let wordlist = [
-  [], [], [], [], [],
-  [], [], [], [], [],
-  [], [], [], [], [], []
+    [], [], [], [], [],
+    [], [], [], [], [],
+    [], [], [], [], [], []
 ];
 
 openDefaultWordlist("https://raw.githubusercontent.com/keiranking/Phil/master/WL-SP.txt");
@@ -25,136 +25,244 @@ openDefaultWordlist("https://raw.githubusercontent.com/keiranking/Phil/master/WL
 // F U N C T I O N S
 
 function addToWordlist(newWords) {
-  for (i = 0; i < newWords.length; i++) {
-    const word = newWords[i].trim().toUpperCase();
-    if (word.length < wordlist.length) { // Make sure we don't access outside the wordlist array
-      wordlist[word.length].push(word);
+    for (i = 0; i < newWords.length; i++) {
+	const word = newWords[i].trim().toUpperCase();
+	if (word.length < wordlist.length) { // Make sure we don't access outside the wordlist array
+	    wordlist[word.length].push(word);
+	}
     }
-  }
 }
 
 function sortWordlist() {
-  for (let i = 3; i < wordlist.length; i++) {
-    wordlist[i].sort();
-  }
+    for (let i = 3; i < wordlist.length; i++) {
+	wordlist[i].sort();
+    }
+}
+
+// source: https://github.com/jashkenas/underscore/blob/master/underscore.js#L1320
+function isObject(obj) {
+    var type = typeof obj;
+    return type === 'function' || type === 'object' && !!obj;
+};
+function cloneObject(src) {
+    let target = {};
+    for (let prop in src) {
+	if (src.hasOwnProperty(prop)) {
+	    // if the value is a nested object, recursively copy all it's properties
+	    if (isObject(src[prop])) {
+		target[prop] = cloneObject(src[prop]);
+	    } else {
+		target[prop] = src[prop];
+	    }
+	}
+    }
+    return target;
 }
 
 function openWordlist() {
-  document.getElementById("open-wordlist-input").click();
+    document.getElementById("open-wordlist-input").click();
 }
 
 function openWordlistFile(e) {
-  wordlist = [
-    [], [], [], [], [],
-    [], [], [], [], [],
-    [], [], [], [], [], []
-  ];
+    wordlist = [
+	[], [], [], [], [],
+	[], [], [], [], [],
+	[], [], [], [], [], []
+    ];
 
-  const file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-  let reader = new FileReader();
-  reader.onload = function(e) {
-    const words = e.target.result.split(/\s/g);
-    addToWordlist(words);
-    sortWordlist();
-    removeWordlistDuplicates();
-    invalidateSolverWordlist();
-  };
-  reader.readAsText(file);
+    const file = e.target.files[0];
+    if (!file) {
+	return;
+    }
+    let reader = new FileReader();
+    reader.onload = function(e) {
+	const words = e.target.result.split(/\s/g);
+	addToWordlist(words);
+	sortWordlist();
+	removeWordlistDuplicates();
+	invalidateSolverWordlist();
+    };
+    reader.readAsText(file);
 }
 
 function openDefaultWordlist(url) {
-  let textFile = new XMLHttpRequest();
-  textFile.open("GET", url, true);
-  textFile.onreadystatechange = function() {
-    if (textFile.readyState === 4 && textFile.status === 200) {  // Makes sure the document is ready to parse, and it's found the file.
-      const words = textFile.responseText.split(/\s/g);
-      addToWordlist(words);
-      sortWordlist();
-      console.log("Loaded default wordlist.")
+    let textFile = new XMLHttpRequest();
+    textFile.open("GET", url, true);
+    textFile.onreadystatechange = function() {
+	if (textFile.readyState === 4 && textFile.status === 200) {  // Makes sure the document is ready to parse, and it's found the file.
+	    const words = textFile.responseText.split(/\s/g);
+	    addToWordlist(words);
+	    sortWordlist();
+	    console.log("Loaded default wordlist.")
+	}
     }
-  }
-  textFile.send(null);
+    textFile.send(null);
 }
 
 function removeWordlistDuplicates() {
-  for (let i = 3; i < wordlist.length; i++) {
-    if (wordlist[i].length >= 2) {
-      for (let j = wordlist[i].length - 1; j >0; j--) {
-        if (wordlist[i][j] == wordlist[i][j - 1]) {
-          wordlist[i].splice(j, 1);
-        }
-      }
+    for (let i = 3; i < wordlist.length; i++) {
+	if (wordlist[i].length >= 2) {
+	    for (let j = wordlist[i].length - 1; j >0; j--) {
+		if (wordlist[i][j] == wordlist[i][j - 1]) {
+		    wordlist[i].splice(j, 1);
+		}
+	    }
+	}
     }
-  }
 }
 
 function matchFromWordlist(word) {
-  const l = word.length;
-  const actualLettersInWord = word.replace(/-/g, "").length;
-  if (actualLettersInWord >= 1 && actualLettersInWord < l) { // Only search if word isn't completely blank or filled
-    word = word.split(DASH).join("\\w");
-    const pattern = new RegExp(word);
-    let matches = [];
-    for (let i = 0; i < wordlist[l].length; i++) {
-      if (wordlist[l][i].search(pattern) > -1) {
-        matches.push(wordlist[l][i]);
-      }
+    const l = word.length;
+    const actualLettersInWord = word.replace(/-/g, "").length;
+    if (actualLettersInWord >= 1 && actualLettersInWord < l) { // Only search if word isn't completely blank or filled
+	word = word.split(DASH).join("\\w");
+	const pattern = new RegExp(word);
+	let matches = [];
+	for (let i = 0; i < wordlist[l].length; i++) {
+	    if (wordlist[l][i].search(pattern) > -1) {
+		matches.push(wordlist[l][i]);
+	    }
+	}
+	return matches;
+    } else {
+	return [];
     }
-    return matches;
-  } else {
-    return [];
-  }
+}
+
+// Determines whether the given letter in WORD at POS1 appears in
+// at least one word in the given WORDS at letter position POS2.
+// CACHE is a set of previously determined harmonious letters.
+function wordIsHarmonious( word, pos1, words, pos2, cache ) {
+    letter = word.substring( pos1, pos1+1 ).toLowerCase();
+    if( cache.has(letter) ) {
+	return( true );
+    } else {
+	for( i = 0; i < words.length; i++ ) {
+	    if( letter == words[ i ].substring( pos2, pos2+1 ).toLowerCase() ) {
+		cache.add( letter );
+		return( true );
+	    }
+	}
+	return( false );
+    }
 }
 
 function updateMatchesUI() {
-  let acrossMatchList = document.getElementById("across-matches");
-  let downMatchList = document.getElementById("down-matches");
-  acrossMatchList.innerHTML = "";
-  downMatchList.innerHTML = "";
-  const acrossMatches = matchFromWordlist(current.acrossWord);
-  const downMatches = matchFromWordlist(current.downWord);
-  for (i = 0; i < acrossMatches.length; i++) {
-    let li = document.createElement("LI");
-    li.innerHTML = acrossMatches[i].toLowerCase();
-    li.className = "";
-    // li.addEventListener('click', printScore);
-    li.addEventListener('dblclick', fillGridWithMatch);
-    acrossMatchList.appendChild(li);
-  }
-  for (i = 0; i < downMatches.length; i++) {
-    let li = document.createElement("LI");
-    li.innerHTML = downMatches[i].toLowerCase();
-    li.className = "";
-    li.addEventListener('dblclick', fillGridWithMatch);
-    downMatchList.appendChild(li);
-  }
+    let acrossMatchList = document.getElementById("across-matches");
+    let downMatchList = document.getElementById("down-matches");
+    acrossMatchList.innerHTML = "";
+    downMatchList.innerHTML = "";
+    console.log( "showOnlyRecommendations=" + showOnlyRecommendations );
+    const acrossMatches = matchFromWordlist(current.acrossWord);
+    const downMatches = matchFromWordlist(current.downWord);
+
+    let hpos = current.col - current.acrossStartIndex;
+    let vpos = current.row - current.downStartIndex;
+
+    let cache1 = new Set();
+    for (let i = 0; i < acrossMatches.length; i++) {
+	let li = document.createElement("LI");
+	li.innerHTML = acrossMatches[i].toLowerCase();
+	li.className = "";
+	// li.addEventListener('click', printScore);
+	li.addEventListener('dblclick', fillGridWithMatch);
+	let h = wordIsHarmonious( acrossMatches[ i ], hpos, downMatches, vpos, cache1 );
+	if( h ) {
+	    li.setAttribute("class", "recommended");
+	}
+	if( h || !showOnlyRecommendations ) {
+	    acrossMatchList.appendChild(li);
+	}
+    }
+
+    let cache2 = new Set();
+    for (let i = 0; i < downMatches.length; i++) {
+	let li = document.createElement("LI");
+	li.innerHTML = downMatches[i].toLowerCase();
+	li.className = "";
+	li.addEventListener('dblclick', fillGridWithMatch);
+	let h = wordIsHarmonious( downMatches[ i ], vpos, acrossMatches, hpos, cache2 );
+	if( h ) {
+	    li.setAttribute("class", "recommended");
+	} 
+	if( h || !showOnlyRecommendations ) {
+	    downMatchList.appendChild(li);
+	}
+    }
+}
+
+// Set Undo button's state to STATE
+function setUndoButton( state, tooltip ) {
+    console.log( "setUndoButton: setting state = " + state + ", tooltip=\"" + tooltip + "\"" );
+    let undoButton = document.getElementById("undo");
+
+    if( undoButton.getAttribute( "data-state" ) != state ) {
+	console.log( "setUndoButton: toggling button-on" );
+	undoButton.classList.toggle("button-on");
+    }
+
+    undoButton.setAttribute( "data-state",  state );
+    undoButton.setAttribute( "data-tooltip", tooltip );
+}
+
+// Undo the latest action done by fillGridWithMatch()
+function undo() {
+    if( undoStack.length > 0 ) {
+	console.log("undo: undoing puzzle to before last grid fill...");
+	const previousCell = grid.querySelector('[data-row="' + current.row + '"]').querySelector('[data-col="' + current.col + '"]');
+	previousCell.classList.remove("active");
+
+	let undoContext = undoStack.pop();
+	xw = undoContext.xw;
+	current = undoContext.current;
+
+	if( undoStack.length <= 0 ) {
+	    setUndoButton( "off", "No further undo information available" );
+	} else {
+	    let undoContext = undoStack[ undoStack.length-1 ];
+	    setUndoButton( "on", "Undo latest grid fill for \"" + undoContext.fill + "\"");
+	}
+
+	isMutated = true;
+	// updateActiveWords();
+	// updateMatchesUI();
+	updateUI();
+	const currentCell = grid.querySelector('[data-row="' + current.row + '"]').querySelector('[data-col="' + current.col + '"]');
+	currentCell.classList.add("active");
+
+	grid.focus();
+    }
 }
 
 function fillGridWithMatch(e) {
-  const li = e.currentTarget;
-  const fill = li.innerHTML.toUpperCase();
-  const dir = (li.parentNode.id == "across-matches") ? ACROSS : DOWN;
+    const li = e.currentTarget;
+    const fill = li.innerHTML.toUpperCase();
+    const dir = (li.parentNode.id == "across-matches") ? ACROSS : DOWN;
 
-  if (dir == ACROSS) {
-    xw.fill[current.row] = xw.fill[current.row].slice(0, current.acrossStartIndex) + fill + xw.fill[current.row].slice(current.acrossEndIndex);
-    for (let i = current.acrossStartIndex; i < current.acrossEndIndex; i++) {
-      const square = grid.querySelector('[data-row="' + current.row + '"]').querySelector('[data-col="' + i + '"]');
-      square.lastChild.innerHTML = fill[i - current.acrossStartIndex];
+    let undoContext = {};
+    undoContext.xw = cloneObject( xw );
+    undoContext.current = cloneObject( current );
+    undoContext.fill = fill;
+    undoStack.push( undoContext );
+    setUndoButton( "on", "Undo latest grid fill for \"" + fill + "\"" );
+    
+    if (dir == ACROSS) {
+	xw.fill[current.row] = xw.fill[current.row].slice(0, current.acrossStartIndex) + fill + xw.fill[current.row].slice(current.acrossEndIndex);
+	for (let i = current.acrossStartIndex; i < current.acrossEndIndex; i++) {
+	    const square = grid.querySelector('[data-row="' + current.row + '"]').querySelector('[data-col="' + i + '"]');
+	    square.lastChild.innerHTML = fill[i - current.acrossStartIndex];
+	}
+    } else {
+	for (let j = current.downStartIndex; j < current.downEndIndex; j++) {
+	    xw.fill[j] = xw.fill[j].slice(0, current.col) + fill[j - current.downStartIndex] + xw.fill[j].slice(current.col + 1);
+	    const square = grid.querySelector('[data-row="' + j + '"]').querySelector('[data-col="' + current.col + '"]');
+	    square.lastChild.innerHTML = fill[j - current.downStartIndex];
+	}
     }
-  } else {
-    for (let j = current.downStartIndex; j < current.downEndIndex; j++) {
-      xw.fill[j] = xw.fill[j].slice(0, current.col) + fill[j - current.downStartIndex] + xw.fill[j].slice(current.col + 1);
-      const square = grid.querySelector('[data-row="' + j + '"]').querySelector('[data-col="' + current.col + '"]');
-      square.lastChild.innerHTML = fill[j - current.downStartIndex];
-    }
-  }
-  isMutated = true;
-  console.log("Filled '" + li.innerHTML + "' going " + dir);
-  // updateActiveWords();
-  // updateMatchesUI();
-  updateUI();
-  grid.focus();
+    isMutated = true;
+    console.log("Filled '" + li.innerHTML + "' going " + dir);
+    // updateActiveWords();
+    // updateMatchesUI();
+    updateUI();
+    grid.focus();
 }
